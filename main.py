5c58fb4595b3c1bbff65b912a7c9e932fc1018ce
Keita Laimiņa <>
2022-01-20 07:53:03 +0000

    kopējās izmaiņas

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import math
 
 def gridas_izmaksa(cena, linoleja_platums, telpas_platums, telpas_garums):
     telpas_izmers = math.ceil(telpas_garums) * math.ceil(telpas_platums)
-    izmaksa = cena * telpas_izmers / linoleja_platums
+    ruli = math.ceil(telpas_platums) / math.ceil(linoleja_platums)
+    izmaksa = ruli*linoleja_platums*telpas_garums*cena
 
+    
     return izmaksa
+  
 
 cena1 = float(input("Ievadiet linoleja cenu: "))
 linoleja_platums1 = float(input("Ievadiet linoleja platumu: "))
 platums1 = float(input("Ievadi telpas platumu: "))
 garums1 = float(input("Ievadiet telpas garumu: "))
 
-print("izklājot garumā:" )
+
+print("izklājot garumā:")
 print(gridas_izmaksa(cena1, linoleja_platums1, platums1, garums1))
 print("izklājot platumā:")
 print(gridas_izmaksa(cena1, linoleja_platums1, garums1, platums1))
